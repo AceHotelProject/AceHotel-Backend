a46e68a4f7a50a86c6adc6c56ea0f2caecd1bf20
sosismayo <ramadani.dwicahya@gmail.com>
2023-12-01 15:43:28 +0700

    fix : Update .gitignore

--- src/config/gcs.js
+++
@@ -1,9 +0,0 @@
-const { Storage } = require('@google-cloud/storage');
-const path = require('path');
-
-const storage = new Storage({
-  keyFilename: path.join(__dirname, '../../credentials/ace-hotel-app-7faad1bbd30a.json'), // Replace with your credentials file path
-  projectId: 'ace-hotel-app', // Replace with your Google Cloud project ID
-});
-
-module.exports = storage;
